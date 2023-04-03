842e3ff7878b5dae7f791330c6326bdeaf72dbad
zakacoding <zakanoor@outlook.co.id>
2023-04-03 09:48:46 +0700

    update: add new javascript library for develop tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,21 @@
 import './bootstrap';
 
+import 'flowbite';
+
+import { Tabs } from 'flowbite';
+
 import Alpine from 'alpinejs';
 
+/**
+ * Alpine
+ */
 window.Alpine = Alpine;
 
 Alpine.start();
+
+/*
+* tabElements: array of tab objects
+* options: optional
+*/
+const tabs = new Tabs(tabElements);
+
