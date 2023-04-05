dfe4864dbf616432c88eecd6a9ff27c31c549979
zakacoding <zakanoor@outlook.co.id>
2023-04-05 11:04:40 +0700

    update: make improvment on Node, User can easly create link between note, and redesign

--- resources/js/cmap.js
+++ resources/js/cmap.js
@@ -6,14 +6,25 @@ function init() {
     // For details, see https://gojs.net/latest/intro/buildingObjects.html
     const $ = go.GraphObject.make;  // for conciseness in defining templates
 
+    // some constants that will be reused within templates
+    let roundedRectangleParams = {
+      parameter1: 2,  // set the rounded corner
+      spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight  // make content go all the way to inside edges of rounded corners
+    };
+
     let myDiagram =
       $(go.Diagram, "canvas",  // must name or refer to the DIV HTML element
         {
           initialAutoScale: go.Diagram.Uniform,  // an initial automatic zoom-to-fit
-          contentAlignment: go.Spot.Center,  // align document to the center of the viewport
+          // contentAlignment: go.Spot.Center,  // align document to the center of the viewport
           layout:
             $(go.ForceDirectedLayout,  // automatically spread nodes apart
-              { maxIterations: 200, defaultSpringLength: 30, defaultElectricalCharge: 100 }),
+              { 
+                maxIterations: 100, 
+                defaultSpringLength: 50, 
+                defaultElectricalCharge: 100,
+              }
+            ),
           
           // have mouse wheel events zoom in and out instead of scroll up and down
           "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
@@ -33,39 +44,60 @@ function init() {
       $(go.Node, "Auto",  // the whole node panel
         { locationSpot: go.Spot.Center },
         // define the node's outer shape, which will surround the TextBlock
-        $(go.Shape, "Rectangle",
-          { fill: $(go.Brush, "Linear", { 0: "rgb(254, 201, 0)", 1: "rgb(254, 162, 0)" }), stroke: "black" }),
+        $(go.Shape, "RoundedRectangle",
+          { 
+            fill:"#537FE7", 
+            stroke: null,
+            portId: "",  // this Shape is the Node's port, not the whole Node
+            fromLinkable: true, fromLinkableSelfNode: true, fromLinkableDuplicates: true,
+            toLinkable: true, toLinkableSelfNode: true, toLinkableDuplicates: true,
+            cursor: "pointer"
+          }
+        ),
         $(go.TextBlock,
           { 
-            font: "bold 10pt helvetica, bold arial, sans-serif", 
+            font: "bold 10pt helvetica, bold arial, sans-serif",
+            stroke: "white",
             margin: 4,
             editable: true  // enable in-place editing
           },
-          new go.Binding("text", "text"))
+          new go.Binding("text").makeTwoWay())
       );
 
     // replace the default Link template in the linkTemplateMap
     myDiagram.linkTemplate =
       $(go.Link,  // the whole link panel
+        {
+          curve: go.Link.Bezier,
+          adjusting: go.Link.Stretch,
+          reshapable: true, relinkableFrom: true, relinkableTo: true,
+          toShortLength: 3
+        },
+        new go.Binding("points").makeTwoWay(),
+        new go.Binding("curviness"),
         $(go.Shape,  // the link shape
-          { stroke: "black" }),
+          { strokeWidth: 1.5 },
+          new go.Binding('stroke', 'progress', progress => progress ? "#52ce60" /* green */ : 'black'),
+          new go.Binding('strokeWidth', 'progress', progress => progress ? 2.5 : 1.5)),
         $(go.Shape,  // the arrowhead
-          { toArrow: "standard", stroke: null }),
+          { toArrow: "standard", stroke: null },
+          new go.Binding('fill', 'progress', progress => progress ? "#52ce60" /* green */ : 'black')),
         $(go.Panel, "Auto",
           $(go.Shape,  // the label background, which becomes transparent around the edges
             {
-              fill: $(go.Brush, "Radial", { 0: "rgb(240, 240, 240)", 0.3: "rgb(240, 240, 240)", 1: "rgba(240, 240, 240, 0)" }),
+              fill: $(go.Brush, "Radial",
+                { 0: "rgb(245, 245, 245)", 0.7: "rgb(245, 245, 245)", 1: "rgba(245, 245, 245, 0)" }),
               stroke: null
             }),
-          $(go.TextBlock,  // the label text
+          $(go.TextBlock, "Link",  // the label text
             {
               textAlign: "center",
-              font: "10pt helvetica, arial, sans-serif",
-              stroke: "#555555",
+              font: "9pt helvetica, arial, sans-serif",
               margin: 4,
               editable: true  // enable in-place editing
             },
-            new go.Binding("text", "text"))
+            // editing the text automatically updates the model data
+            new go.Binding("text").makeTwoWay())
         )
       );
 
@@ -120,4 +152,43 @@ function init() {
     ];
     myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
   }
+
+  // clicking the button inserts a new node to the right of the selected node,
+  // and adds a link to that new node
+  function addNodeAndLink(e, obj) {
+    var adornment = obj.part;
+    var diagram = e.diagram;
+    diagram.startTransaction("Add State");
+
+    // get the node data for which the user clicked the button
+    var fromNode = adornment.adornedPart;
+    var fromData = fromNode.data;
+    // create a new "State" data object, positioned off to the right of the adorned Node
+    var toData = { text: "new" };
+    var p = fromNode.location.copy();
+    p.x += 200;
+    toData.loc = go.Point.stringify(p);  // the "loc" property is a string, not a Point object
+    // add the new node data to the model
+    var model = diagram.model;
+    model.addNodeData(toData);
+
+    // create a link data from the old node data to the new node data
+    var linkdata = {
+      from: model.getKeyForNodeData(fromData),  // or just: fromData.id
+      to: model.getKeyForNodeData(toData),
+      text: "transition"
+    };
+    // and add the link data to the model
+    model.addLinkData(linkdata);
+
+    // select the new Node
+    var newnode = diagram.findNodeForData(toData);
+    diagram.select(newnode);
+
+    diagram.commitTransaction("Add State");
+
+    // if the new node is off-screen, scroll the diagram to show the new node
+    diagram.scrollToRect(newnode.actualBounds);
+  }
+
   window.addEventListener('DOMContentLoaded', init);
