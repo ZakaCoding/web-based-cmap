b1bfa723e2387975a0693f6b8bd6d9f0a54fbb5c
zakacoding <zakanoor@outlook.co.id>
2023-05-06 14:18:20 +0700

    update: add data-user attribute for integrated js and laravel API

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,10 +23,6 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-
-                    {{-- <x-nav-link :href="route('project')" :active="request()->routeIs('project')">
-                        {{ __('Projects') }}
-                    </x-nav-link> --}}
                 </div>
             </div>
 
@@ -35,7 +31,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div id="user" data-user="{{ json_encode(Auth::user()) }}">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
