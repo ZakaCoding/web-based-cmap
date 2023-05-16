46f674da8647ef3418cc605d794147ead7457a16
zakacoding <zakanoor@outlook.co.id>
2023-05-16 17:51:53 +0700

    update: change action button and add share link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,7 +49,7 @@
                     <p class="font-light text-gray-600">Browse a list of Concept Mapping you have been create.</p>
                 </div>
 
-                <div class="relative overflow-x-auto shadow-md rounded-lg">
+                <div class="relative shadow-sm rounded-lg">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                             List all concept map
@@ -69,11 +69,8 @@
                                 <th scope="col" class="px-6 py-3">
                                     Date Upload
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="px-6 py-3" colspan="2">
                                     Last Update
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Action
                                 </th>
                             </tr>
                         </thead>
@@ -120,7 +117,31 @@
                                     {{ date('M d, Y. h:iA', strtotime($concept->updated_at)) }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                                    <button class="w-6 h-6" data-dropdown-toggle="dropdownAction{{ $concept->id }}">
+                                        <svg class="stroke-2" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"></path>
+                                        </svg>
+                                    </button>
+                                    {{-- Dropdown action --}}
+                                    <div id="dropdownAction{{ $concept->id }}" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                        <ul class="text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                          <li class="flex items-center justify-content-center px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                                            </svg>
+                                            <a href="#" class="block px-4 py-2">Delete</a>
+                                          </li>
+                                          <li class="flex items-center justify-content-center px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"></path>
+                                            </svg>
+                                            <a href="{{ url('/concept-map/'.$concept->key) }}" class="block px-4 py-2">Share</a>
+                                          </li>
+                                        </ul>
+                                        {{-- <div class="">
+                                          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
+                                        </div> --}}
+                                    </div>
                                 </td>
                             </tr>
                             @endforeach
@@ -214,7 +235,7 @@
                     <p class="font-light text-gray-600">Browse a list of Assasment that you have done to take.</p>
                 </div>
 
-                <div class="relative overflow-x-auto shadow-md rounded-lg">
+                <div class="relative overflow-x-auto shadow-sm rounded-lg">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                             List Assasment
@@ -266,7 +287,6 @@
     <script>
         function load(key)
         {
-
             // call laravel API to load data model
             let url = '/api/load-data-assignment/' + key
             fetch(url, {
