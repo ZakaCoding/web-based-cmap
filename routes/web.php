020c7f721671ecf300bb54320c4b18158355665a
zakacoding <zakanoor@outlook.co.id>
2023-06-09 14:44:47 +0700

    add: release notes page

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::get(
     [DashboardController::class, 'index']
 )->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get(
+    '/release-notes',
+    [DashboardController::class, 'releasePages']
+)->middleware(['auth', 'verified'])->name('dashboard.release');
+
 // Route::name('project.')->prefix('/project')->middleware(['auth', 'verified'])->group(function () {
 // });
 
